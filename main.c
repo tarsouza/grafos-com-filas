1b76bb78b5f27dbd7ab83d0785e196d794bc830e
tarsouza <71481489+tarsouza@users.noreply.github.com>
2021-12-20 18:48:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,15 @@
 #include <stdlib.h>
 #include <locale.h>
 #include "filas.h"
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "Portuguese");
+    
     Matriz m1;
-
-	Pilha p1;
-    inicializa_pilha( &p1, 32 );
-	
-	empilha( &p1, 1);
-
-    carrega_matriz_arquivo( "arquivotexto.txt", &m1 );
+    carrega_matriz_arquivo( "arquivotexto.txt", &m1 );		// importando matriz 8x8 que está em .txt
     mostra_matriz( m1 );
 
-    int *vs = aloca_vetor_zerado(m1.col);
-    int *vant = aloca_vetor_antecessor(m1.col);
-    
 	int ini;
 	printf("Digite o vertice inicial: ");
 	scanf("%i", &ini);
@@ -26,22 +19,49 @@ int main(int argc, char *argv[]) {
 	printf("Digite o vertice final: ");
 	scanf("%i", &fin);
 
+    int *vs = aloca_vetor_zerado(m1.col);
+    int *vant = aloca_vetor_antecessor(m1.col);
 
 	Fila f1;
 	inicializa_fila( &f1 );
 	mostra_fila( f1 );
-	
-	inserir( &f1, 2 );
-	mostra_fila( f1 );
-	
-	int x;
-	remover( &f1, &x );
-	printf("Valor removido: %d\n\n", x );
-	mostra_fila( f1 );
-	
-	mostra_pilha (p1);
 
-    desaloca_pilha( &p1 );
+	vs[ini]= 1;
+
+	inserir( &f1, ini );
+	
+	int achou = 0;
+	int i, x;
+	int dimensao = 8;
+
+		while(fila_vazia(f1) != 1 && achou == 0){
+			if(x==fin){
+				achou = 1;
+			} else {
+				for (i = 0; i < dimensao; i++){
+					if(vs[i]==0){
+						
+						vs[i]= 1;
+						vant[i]= x;
+						inserir( &f1, i );
+
+					}
+				}
+			}
+		} if( achou == 1){
+			Pilha p1;
+    		inicializa_pilha( &p1, 32 );
+    		
+    		while( x!= 0){
+    			empilha(&p1, x);
+    			x = vant[x];
+			}while(pilha_vazia(p1) != -1){
+				desempilha(&p1, x);
+				printf("%i", x);
+			}
+		}else{
+			printf("B Não é alcançável a partir de A");
+		}
 	
     return 0; 
 }
